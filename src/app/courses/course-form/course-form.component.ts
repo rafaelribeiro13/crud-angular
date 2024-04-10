bb552abfe3e94f936393d367132be31a25537715
Rafael Ribeiro <rafaelsribeiro45@gmail.com>
2024-04-09 22:20:44 -0300

    implementação de formulário para criar cursos

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-course-form',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./course-form.component.scss']
 })
 export class CourseFormComponent {
+
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: [null],
+      category: [null]
+    });
+  }
+
+  onSubmit(): void {
+    console.log('onSubmit()');
+  }
+
+  onCancel(): void {
+    console.log('onCancel()');
+  }
 
 }
