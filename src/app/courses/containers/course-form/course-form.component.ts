c1f1badfb8bdb594470ed8a67dcdd3432a14c918
Rafael Ribeiro <rafaelsribeiro45@gmail.com>
2024-04-13 11:23:38 -0300

    populando formulário na rota de editar/atualizar cursos

--- src/app/courses/containers/course-form/course-form.component.ts
+++ src/app/courses/containers/course-form/course-form.component.ts
@@ -1,17 +1,20 @@
 import { Location } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NonNullableFormBuilder, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { CourseService } from '../../services/course.service';
+import { ActivatedRoute } from '@angular/router';
+import { ICourse } from '../../models/course';
 
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
   styleUrls: ['./course-form.component.scss']
 })
-export class CourseFormComponent {
+export class CourseFormComponent implements OnInit {
 
   form = this.fb.group({
+    _id: [''],
     name: ['', [Validators.required]],
     category: ['', [Validators.required]]
   });
@@ -20,8 +23,19 @@ export class CourseFormComponent {
     private fb: NonNullableFormBuilder,
     private service: CourseService,
     private _snackBar: MatSnackBar,
-    private location: Location
+    private location: Location,
+    private route: ActivatedRoute
   ) {}
+
+  ngOnInit(): void {
+    const course: ICourse = this.route.snapshot.data['course'];
+    
+    this.form.setValue({
+      _id: course._id,
+      name: course.name,
+      category: course.category
+    });
+  }
 
   onSubmit(): void {
     this.service.save(this.form.value).subscribe({
