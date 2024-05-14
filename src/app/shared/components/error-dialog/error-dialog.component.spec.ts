cb20e5b2e7d9303f086e34e199f8fed530426ee4
Rafael Ribeiro <rafaelsribeiro45@gmail.com>
2024-05-13 21:07:10 -0300

    teste unitário ErrorDialogComponent

--- src/app/shared/components/error-dialog/error-dialog.component.spec.ts
+++ src/app/shared/components/error-dialog/error-dialog.component.spec.ts
@@ -1,15 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ErrorDialogComponent } from './error-dialog.component';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 
 describe('ErrorDialogComponent', () => {
   let component: ErrorDialogComponent;
   let fixture: ComponentFixture<ErrorDialogComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [ErrorDialogComponent]
-    });
+  beforeEach(async () => {
+
+    await TestBed.configureTestingModule({
+      declarations: [
+        ErrorDialogComponent
+      ],
+      imports: [
+        MatDialogModule
+      ],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: 'Error' },
+        { provide: MatDialogRef, useValue: {} }
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ErrorDialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -18,4 +30,16 @@ describe('ErrorDialogComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should launch an error dialog with a message and a Close button', () => {
+    const errorMessageDom = fixture.nativeElement.querySelector(
+      '.mat-mdc-dialog-content'
+    );
+
+    expect(errorMessageDom.textContent).toContain('Error');
+
+    const okBtn = fixture.nativeElement.querySelector('button');
+    expect(okBtn.textContent).toContain('Fechar');
+  });
+
 });
